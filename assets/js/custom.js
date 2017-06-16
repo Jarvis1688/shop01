9272b0e418b42585f5688528c13ae07144cfa2f0
Jarvis1688 <378109196@qq.com>
2017-06-16 15:15:01 +0800

    test ajax

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,33 +1,6 @@
 // 访问本地数据
-// $(document).ready(function(){
-//     $.getJSON("./in_theaters.json",  
-//         function (data) {  
-           
-//             data = data["subjects"];
-//             for(var item in data){
-//                  var list = '';
-//                 list += "<li class=\"list-group-item\">";
-//                 list +=        "<div class=\"row\">";
-//                 list +=            "<div class=\"col-md-2\"><img src=\""+ data[item].casts[0].avatars.small+"\"></div>";
-//                 list +=            "<div class=\"col-md-10\"><h3>"+ data[item].title +"</h3><p> 类型： "+ data[item].genres +"</p><p> 上映时间： "+ data[item].year +"</p></div>";
-//                 list +=        "</div>" ;
-//                 list += "</li>";
-//             $("#movie_list").append(list);
-//                 // console.log(list);
-//             }
-//         }  
-//     );
-
-    
-// });
-
-
-
-
-
-// 访问远程数据
 $(document).ready(function(){
-   $.getJSON("http://api.douban.com/v2/movie/in_theaters?start=1&count=3&callback=?",  
+    $.getJSON("./in_theaters.json",  
         function (data) {  
            
             data = data["subjects"];
@@ -47,6 +20,33 @@ $(document).ready(function(){
 
     
 });
+
+
+
+
+
+// 访问远程数据
+// $(document).ready(function(){
+//    $.getJSON("http://api.douban.com/v2/movie/in_theaters?start=1&count=3&callback=?",  
+//         function (data) {  
+           
+//             data = data["subjects"];
+//             for(var item in data){
+//                  var list = '';
+//                 list += "<li class=\"list-group-item\">";
+//                 list +=        "<div class=\"row\">";
+//                 list +=            "<div class=\"col-md-2\"><img src=\""+ data[item].casts[0].avatars.small+"\"></div>";
+//                 list +=            "<div class=\"col-md-10\"><h3>"+ data[item].title +"</h3><p> 类型： "+ data[item].genres +"</p><p> 上映时间： "+ data[item].year +"</p></div>";
+//                 list +=        "</div>" ;
+//                 list += "</li>";
+//             $("#movie_list").append(list);
+//                 // console.log(list);
+//             }
+//         }  
+//     );
+
+    
+// });
 
 
 // 测试github上的豆瓣数据
