199bdf46645cc4bf978e6961b77ccff4f41aa988
Jarvis1688 <378109196@qq.com>
2017-06-16 12:46:58 +0800

    提交json数据

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,13 @@
-/* Write here your custom javascript codes */
+
+    //  alert(333);  
+    // $.getJSON("http://api.douban.com/v2/movie/in_theaters",  
+    //     function (data) {  
+    //         alert(1111);  
+    //     }  
+    // );
+
+
+
+
+
+
