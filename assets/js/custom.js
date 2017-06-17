ea0748dcdcb796816e0c8e6e79c6763298533d1a
Jarvis1688 <378109196@qq.com>
2017-06-17 22:06:20 +0800

    移动项目位置

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,27 +26,27 @@
 
 
 // 访问远程数据
-$(document).ready(function(){
-   $.getJSON("http://api.douban.com/v2/movie/in_theaters?start=1&count=3&callback=?",  
-        function (data) {  
+// $(document).ready(function(){
+//    $.getJSON("http://api.douban.com/v2/movie/in_theaters?start=1&count=3&callback=?",  
+//         function (data) {  
            
-            data = data["subjects"];
-            for(var item in data){
-                 var list = '';
-                list += "<li class=\"list-group-item\">";
-                list +=        "<div class=\"row\">";
-                list +=            "<div class=\"col-md-2\"><img src=\""+ data[item].casts[0].avatars.small+"\"></div>";
-                list +=            "<div class=\"col-md-10\"><h3>"+ data[item].title +"</h3><p> 类型： "+ data[item].genres +"</p><p> 上映时间： "+ data[item].year +"</p></div>";
-                list +=        "</div>" ;
-                list += "</li>";
-            $("#movie_list").append(list);
-                // console.log(list);
-            }
-        }  
-    );
+//             data = data["subjects"];
+//             for(var item in data){
+//                  var list = '';
+//                 list += "<li class=\"list-group-item\">";
+//                 list +=        "<div class=\"row\">";
+//                 list +=            "<div class=\"col-md-2\"><img src=\""+ data[item].casts[0].avatars.small+"\"></div>";
+//                 list +=            "<div class=\"col-md-10\"><h3>"+ data[item].title +"</h3><p> 类型： "+ data[item].genres +"</p><p> 上映时间： "+ data[item].year +"</p></div>";
+//                 list +=        "</div>" ;
+//                 list += "</li>";
+//             $("#movie_list").append(list);
+//                 // console.log(list);
+//             }
+//         }  
+//     );
 
     
-});
+// });
 
 
 // 测试github上的豆瓣数据
